cc5846a7f6994d0a8fdb4ad25712050e9e44d232
bayapucg <bayapu.edu004@gmail.com>
2019-01-08 17:40:07 +0530

    commit
    cahgnes

--- register.php
+++ register.php
@@ -6,7 +6,6 @@
     <title>Sri Raja Bags</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="OneTech shop project">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="css/bootstrap4/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap4/bootstrapValidator.min.css">
